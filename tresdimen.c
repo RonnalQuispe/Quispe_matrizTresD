c1493b85f8cdf1520f82349e6a906de40fee1a13
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-12-04 22:02:25 -0500

    guardamos las dimensiones de la matriz en la matris llamada -tresD-

--- tresdimen.c
+++ tresdimen.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int x, y, z;
-    printf("Ingrese las dimensiones del arreglo tridimensional:\n");
+    printf("Ingrese las dimensiones del arreglo tridimensional:\n"); // pedimos al usuario dimesiones de la matriz
     printf("Primera dimension:");
     scanf("%d", &x);
     printf("Segunda dimension:");
     scanf("%d", &y);
     printf("Tercera dimension:");
     scanf("%d", &z);
+    int arreglo[x][y][z];
 
-    
+   
     return 0;
 }
