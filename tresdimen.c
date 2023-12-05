92f09e463fa4fa52a766f123f883c42e00379aab
Ronny Quispe <ronny.quispe@udla.edu.ec>
2023-12-04 21:54:54 -0500

    estructur basica de un programa solo con la libreria stdio.h

--- tresdimen.c
+++ tresdimen.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    int x, y, z;
+    printf("Ingrese las dimensiones del arreglo tridimensional:\n");
+    printf("Primera dimension: ");
+    scanf("%d", &x);
+    printf("Segunda dimension: ");
+    scanf("%d", &y);
+    printf("Tercera dimension: ");
+    scanf("%d", &z);
+
     
     return 0;
 }
